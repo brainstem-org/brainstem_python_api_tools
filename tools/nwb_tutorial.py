59b1f6692f1ae7ad723803045be988fb4b3e33bc
weiglszonja <wszonja@gmail.com>
2023-05-23 16:50:08 +0200

    update tutorial

--- tools/nwb_tutorial.py
+++ tools/nwb_tutorial.py
@@ -1,9 +1,15 @@
 from brainstem_api_client import BrainstemClient
-from tools.nwb import get_dataset_metadata, get_nwbfile_metadata
+from tools.nwb import get_dataset_metadata, write_conversion_specification_yml
 
 # initialize token
 client = BrainstemClient(token="")
 
 dataset_id = "160caf7b-cee7-4034-8529-aee846c4dc71"
 dataset_metadata = get_dataset_metadata(client=client, dataset_id=dataset_id)
-nwbfile_metadata = get_nwbfile_metadata(dataset_metadata=dataset_metadata)
+
+yml_file_path = "output.yaml"
+write_conversion_specification_yml(
+    yml_file_path=yml_file_path,
+    dataset_metadata=dataset_metadata,
+    conversion_options=dict(stub_test=True),
+)
