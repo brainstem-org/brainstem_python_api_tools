72292dc91307e214fd0feba56bbc344f4aff5d03
weiglszonja <wszonja@gmail.com>
2023-05-24 15:34:54 +0200

    refactor

--- tools/nwb.py
+++ tools/nwb.py
@@ -5,21 +5,9 @@ import yaml
 from bs4 import BeautifulSoup
 from dateutil.parser import parse
 from neuroconv.datainterfaces import interfaces_by_category
+from neuroconv.utils import FilePathType
 
 from brainstem_api_client import BrainstemClient
-
-ecephys_interfaces = interfaces_by_category["ecephys"]
-supplier_to_ecephys_interface = defaultdict(None)
-supplier_to_ecephys_interface.update(
-        BlackrockMicrosystems=ecephys_interfaces.get("Blackrock").__name__,
-        CEDCambridgeElectronicdesignlimited=ecephys_interfaces.get("CED").__name__,
-        IMEC=ecephys_interfaces.get("SpikeGLX").__name__,
-        IntanTechnologies=ecephys_interfaces.get("Intan").__name__,
-        JaneliaResearchCampus=ecephys_interfaces.get("SpikeGLX").__name__,
-        Neuralynx=ecephys_interfaces.get("Neuralynx").__name__,
-        OpenEphys=ecephys_interfaces.get("OpenEphys").__name__,
-        PlexonInstruments=ecephys_interfaces.get("Plexon").__name__,  # sorting or recording?
-    )
 
 
 def get_project_metadata(client: BrainstemClient, project_id: str, portal: str = "public") -> Dict:
@@ -96,70 +84,140 @@ def get_dataset_metadata(client: BrainstemClient, dataset_id: str, portal: str =
     return dataset_metadata
 
 
+def get_ecephys_interface_from_supplier(supplier: str):
+    supplier_to_interface = defaultdict()
+    supplier_to_interface.update(
+        BlackrockMicrosystems="Blackrock",
+        CEDCambridgeElectronicdesignlimited="CED",
+        IMEC="SpikeGLX",
+        IntanTechnologies="Intan",
+        JaneliaResearchCampus="SpikeGLX",
+        Neuralynx="Neuralynx",
+        OpenEphys="OpenEphys",
+        PlexonInstruments="Plexon",  # sorting or recording?
+        ),
+    # note to remove special characters (e.g. re.sub(r'[^A-Za-z0-9\s]+', '', supplier))
+    supplier = supplier.replace(" ", "")
+    return supplier_to_interface.get(supplier)
+
+
 def write_conversion_specification_yml(
-        yml_file_path: str,
+        yml_file_path: FilePathType,
         dataset_metadata: Dict,
-        interface_kwargs: Optional[Dict] = None,
+        interface_properties: Optional[Dict] = None,
         conversion_options: Optional[Dict] = None,
 ):
+    """
+    Write conversion specification to a yaml file.
+
+    The resulting YAML file will follow the structure outlined below:
+
+   ```
+    conversion_options:
+        stub_test: true
+    data_interfaces:
+        recording: OpenEphysRecordingInterface
+    experiments:
+        ecephys:
+            metadata:
+                NWBFile:
+                    session_description: asd
+            sessions:
+                - metadata:
+                    NWBFile:
+                        identifier: 160caf7b-cee7-4034-8529-aee846c4dc71
+                        session_id: TestSession1
+                        session_start_time: '2023-05-15T14:30:00+00:00'
+                    Subject:
+                        date_of_birth: '2023-03-01'
+                        sex: F
+                        species: Mus musculus
+                        strain: C57B1/6
+                        subject_id: TestS1
+                nwbfile_name: TestSession1.nwb
+                source_data:
+                    recording:
+                        folder_path: openephys/OpenEphys_SampleData_1
+                        stream_name: Ch
+    metadata:
+        NWBFile:
+            experiment_description: Project description
+    ```
+
+    Parameters
+    ----------
+    yml_file_path : FilePathType
+        The destination file path where the .yml specification file will be created.
+    dataset_metadata : Dict
+        The metadata for BrainSTEM dataset.
+        The dataset is required to contain the start time of the recording.
+    interface_properties : Dict, optional
+        The interface specific properties passed as a dictionary (e.g. dict(stream_name="Signals CH").
+    conversion_options: Dict, optional
+        The global conversion options passed as a dictionary (e.g. dict(stub_test=True)).
+    """
     assert dataset_metadata["date_time"], "The 'date_time' field should contain the start time of the recording."
 
-    conversion_spec = dict(metadata=dict(NWBFile=dict()))
+    conversion_spec = dict(
+        metadata=dict(),
+        experiments=dict(),
+        data_interfaces=dict(),
+        conversion_options=dict(),
+    )
+    # Add global NWBFile metadata (e.g. experiment_description)
     experiment_description_html = dataset_metadata["project"]["description"]
     if experiment_description_html:
         experiment_description = BeautifulSoup(experiment_description_html, "html.parser").get_text()
-        conversion_spec["metadata"]["NWBFile"].update(experiment_description=experiment_description)
+        conversion_spec["metadata"].update(NWBFile=dict(experiment_description=experiment_description))
 
+    # Add global conversion options (e.g. stub_test)
     if conversion_options:
         conversion_spec.update(dict(conversion_options=conversion_options))
 
     experiment_data = dataset_metadata["experiment_data"]
+    # Add interface specific source data based on experiment_data
     source_data = dict(recording=dict())
-
     if experiment_data["type"] == "Extracellular":
-        # note to remove special characters (e.g. re.sub(r'[^A-Za-z0-9\s]+', '', supplier))
-        supplier = experiment_data["supplier"]["name"].replace(" ", "")
-        interface_name = supplier_to_ecephys_interface.get(supplier)
-        if interface_name is not None:
-            conversion_spec.update(dict(data_interfaces=dict(recording=interface_name)))
+        supplier_name = experiment_data["supplier"]["name"]
+        interface_name = get_ecephys_interface_from_supplier(supplier=supplier_name)
+        ecephys_interfaces = interfaces_by_category["ecephys"]
+        interface_cls = ecephys_interfaces.get(interface_name)
+        assert interface_cls, f"Could not find a matching interface for {supplier_name} supplier."
+        conversion_spec["data_interfaces"].update(recording=interface_cls.__name__)
 
-            if interface_kwargs:
-                # e.g. specifying the stream_name
-                source_data.update(recording=interface_kwargs)
-
-        conversion_spec.update(experiments=dict(ecephys=dict()))
-        session_metadata = dict(NWBFile=dict(session_description=experiment_data["description"]))
-
+        interface_source_schema = interface_cls.get_source_schema()
+        required_properties = interface_source_schema["required"]  # file_path or folder_path
         # limited for now to use only first path
         path = dataset_metadata["data_repository"]["data_protocols_json"][0]["path"]
-        # TODO: interface specific folder path or file path
-        source_data["recording"].update(folder_path=path)  # relative folder path
+        source_data["recording"][required_properties[0]] = path
 
-        sessions = [
-            dict(
-                nwbfile_name=f"{dataset_metadata['name']}.nwb",
-                source_data=source_data,
-                metadata=dict(
-                    NWBFile=dict(
-                        session_id=dataset_metadata["name"],
-                        session_start_time=parse(dataset_metadata["date_time"]).isoformat(),
-                        identifier=dataset_metadata["id"],
-                    ),
-                    Subject=dict(
-                        subject_id=dataset_metadata["subject"]["name"],
-                        strain=dataset_metadata["subject"]["strain"]["name"],
-                        sex=dataset_metadata["subject"]["sex"],
-                        species=dataset_metadata["subject"]["species"]["description"].capitalize(),
-                        date_of_birth=dataset_metadata["subject"]["birth_date"],
-                    ),
-                )
-            )
-        ]
+        if interface_properties:
+            # e.g. specifying the stream_name
+            source_data["recording"].update(interface_properties)
 
-        conversion_spec["experiments"]["ecephys"].update(
-            metadata=session_metadata,
-            sessions=sessions,
+        sessions = []
+        ecephys_session_metadata = dict(
+            nwbfile_name=f"{dataset_metadata['name']}.nwb",
+            source_data=source_data,
+            metadata=dict(
+                NWBFile=dict(
+                    session_id=dataset_metadata["name"],
+                    session_start_time=parse(dataset_metadata["date_time"]).isoformat(),
+                    identifier=dataset_metadata["id"],
+                ),
+                Subject=dict(
+                    subject_id=dataset_metadata["subject"]["name"],
+                    strain=dataset_metadata["subject"]["strain"]["name"],
+                    sex=dataset_metadata["subject"]["sex"],
+                    species=dataset_metadata["subject"]["species"]["description"].capitalize(),
+                    date_of_birth=dataset_metadata["subject"]["birth_date"],
+                ),
+            ),
         )
+        sessions.append(ecephys_session_metadata)
+
+        metadata = dict(NWBFile=dict(session_description=experiment_data["description"]))
+        conversion_spec["experiments"].update(ecephys=dict(metadata=metadata, sessions=sessions))
 
     with open(yml_file_path, 'w') as file:
         yaml.dump(conversion_spec, file, default_flow_style=False)
